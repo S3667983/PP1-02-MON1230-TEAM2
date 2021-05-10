bdb592ef1b1a913623d8f797a9cfaa09efa1e1c2
S3667983 <61996941+S3667983@users.noreply.github.com>
2021-05-10 14:07:11 +1000

    Add files via upload

---
+++ public_html/includes/footer.php
@@ -0,0 +1,15 @@
+<html>
+    <body>
+        <footer>
+            <div class= "container">
+            <nav>
+                <ul>
+                    <li><a href="#">Admin Login</a></li>
+                    <li><a href="#">About us</a></li>
+                    <li><a href="#">Contact us</a></li>
+                </ul>
+            </nav>
+            </div>
+        </footer>
+    </body>
+</html>
---
+++ public_html/includes/header.php
@@ -0,0 +1,18 @@
+<html>
+    <body>
+        <header>
+            <div class="container">
+                <img src="img/logo.png" alt="logo" class="logo">
+                <nav>
+                    <ul>
+                        <li><a href="#">Car/ Rental Information</a></li>
+                        <li><a href="booking.php">Create Booking</a></li>
+                        <li><a href="managebooking.php">Manage Booking</a></li>
+                        <li><a href="#">Location</a></li>
+                        <li><a href="<?php echo $statuspage ?>"><?php echo $status ?></a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    </body>
+</html>
