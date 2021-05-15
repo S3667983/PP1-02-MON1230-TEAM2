f72df495198a99cf308179da8928853ee3014375
S3667983 <61996941+S3667983@users.noreply.github.com>
2021-05-15 21:06:46 +1000

    the new one

--- public_html/adminportal.php
+++ public_html/adminportal.php
@@ -34,22 +34,9 @@ if(isset($_POST['username'])){
     </head>
 
 
-    <header>
-        <div class="container">
-
-            <img src="img/logo.png" alt="logo" class="logo">
-
-            <nav>
-                <ul>
-                    <li><a href="#">Car/ Rental Information</a></li>
-                    <li><a href="#">Create Booking</a></li>
-                    <li><a href="#">Manage Booking</a></li>
-                    <li><a href="#">Location</a></li>
-                    <li><a href="adminportal.php">Admin Portal</a></li>
-                </ul>
-            </nav>
-        </div>
-    </header>
+    <?php
+        include('adminHeader.php');
+    ?>
 
     <div class="wrapper">
 
