7ea441101bc6ce79d1bfc0128fba01d5cc57428b
S3667983 <61996941+S3667983@users.noreply.github.com>
2021-05-15 20:56:15 +1000

    Add files via upload

---
+++ public_html/includes/adminHeader.php
@@ -0,0 +1,16 @@
+    <header>
+        <div class="container">
+
+            <img src="img/logo.png" alt="logo" class="logo">
+
+            <nav>
+                <ul>
+                    <li><a href="#">Car/ Rental Information</a></li>
+                    <li><a href="#">Create Booking</a></li>
+                    <li><a href="#">Manage Booking</a></li>
+                    <li><a href="#">Location</a></li>
+                    <li><a href="adminportal.php">Admin Portal</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
