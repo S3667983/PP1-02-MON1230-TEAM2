90038c1ba9de11b62e5d40b1b0509e4d64d90532
S3667983 <61996941+S3667983@users.noreply.github.com>
2021-05-15 21:09:56 +1000

    Add files via upload

---
+++ public_html/location.php
@@ -0,0 +1,33 @@
+<html>
+
+    <head>
+        <link rel="stylesheet" href="style.css">
+        <meta charset="UTF-8">
+        <title>Car Share Co.</title>
+    </head>
+
+
+    <body>
+
+        <header>
+            <div class="container">
+
+                <img src="img/logo.png" alt="logo" class="logo">
+
+            <?php
+            include('header.php');
+            ?>
+
+        </header>
+
+        <body>
+            <div class="container">
+
+            </div>
+            <?php
+            include('footer.php');
+            ?>
+        </body>
+        
+
+        </html>
