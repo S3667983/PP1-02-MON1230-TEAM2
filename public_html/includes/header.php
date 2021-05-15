8410a8db8e0d62708d2ff62d36a9fb5a884ac86a
S3667983 <61996941+S3667983@users.noreply.github.com>
2021-05-15 21:10:51 +1000

    Add files via upload

--- public_html/includes/header.php
+++ public_html/includes/header.php
@@ -8,7 +8,7 @@
                         <li><a href="#">Car/ Rental Information</a></li>
                         <li><a href="booking.php">Create Booking</a></li>
                         <li><a href="managebooking.php">Manage Booking</a></li>
-                        <li><a href="#">Location</a></li>
+                        <li><a href="location.php">Location</a></li>
                         <li><a href="<?php echo $statuspage ?>"><?php echo $status ?></a></li>
                     </ul>
                 </nav>
